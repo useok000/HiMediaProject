61c981d2f11a9097831af9acef1da7b12ee2f223
us.kwak <us000000@naver.com>
2024-07-17 18:25:09 +0900

    추가

---
+++ project01-1/src/test/java/com/exam/proj01/Project011ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.proj01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project011ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
